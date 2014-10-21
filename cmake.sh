0ecdc417b0fbd7ccf57096c9be19852ee21b2ee5
shak-mar <shakmar@openmailbox.org>
2014-10-21 19:13:14 +0200

    Switch from setup.sh and build.sh to cmake.sh

---
+++ cmake.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# if any command fails, bash will exit with a non-zero exit code
+set -e
+
+case $1 in
+    "setup")
+        cmake -H. -Bbuild
+        ;;
+    "debug")
+        cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug
+        ;;
+    "clean")
+        echo "removing build/ and packman executable"
+        rm -rf build packman
+        ;;
+    *)
+        make -C build $1
+        ;;
+esac
+
+if [ $# -gt 1 ]
+    then
+        shift
+        ./cmake.sh $*
+fi
